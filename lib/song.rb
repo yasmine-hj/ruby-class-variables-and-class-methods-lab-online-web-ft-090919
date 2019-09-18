19a9b83c4f68f2272a7ba8e07d7af4faed24cb3d
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-18 14:38:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,17 @@ class Song
     @@genres.uniq
   end
 
+def self.genre_count
+  genre_count = {}
+  @@genre.each do |genre|
+    if genre_count [genre]
+      genre_count [genre] += 1
+    else
+      genre_count[genre] = 1
+    end
+  end
+  genre_count
+end
   
   def self.artists_count
     artists_count = {}
