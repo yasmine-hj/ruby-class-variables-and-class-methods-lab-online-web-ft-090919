af1acbccfea7833417814bda40533c36e3eb325e
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-18 14:22:04 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count +=1
+  @@artists << artist
+  @@genres << genre
+  
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.artists_count
+    artists_count = []
+    if artist.include? @@artist
+      artists_count +=1
+   else
+      artists_count = 1
+     end
+  artists_count
+end
+end
